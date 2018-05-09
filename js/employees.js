505b20a532a497f0b0a4c84e1e6f1239c9e6102b
Jacob T. Murphy <jacobtrevmurphy@gmail.co.uk>
2018-05-09 16:15:25 +0100

    Apply some base mobile styles

--- js/employees.js
+++ js/employees.js
@@ -1,6 +1,6 @@
 //Function to fetch data & convert it to json.
 function fetchData(url) {
-    return fetch(url).then(res => res.json())
+    return fetch(url).then(res => res.json());
 };
 
 //Fetch calls
@@ -12,12 +12,13 @@ fetchData("https://randomuser.me/api/?results=12&nat=gb")
 
 //Helper Functions
 
+//Function to populate the Employees div with user data.
 function listEmployees(data) {
     const list = data.map(employee => `<div class="card">
-                                            <img src='${employee.picture.medium}' alt>
-                                            <h3 class='name'>${employee.name.first} ${employee.name.last}</h3>
+                                            <img src='${employee.picture.large}' alt>
+                                            <h3 class='name'>${employee.name.first[0].toUpperCase()}${employee.name.first.slice(1)} ${employee.name.last[0].toUpperCase()}${employee.name.last.slice(1)}</h3>
                                             <h4 class="mail">${employee.email}</h4>
-                                            <span class="location">${employee.location.city}</span>
-                                        </div>`);
+                                            <span class="location">${employee.location.city[0].toUpperCase()}${employee.location.city.slice(1)}</span>
+                                        </div>`).join("");
     document.querySelector("#employees").innerHTML = list;
-}
+};
