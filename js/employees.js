c3e26ccd08a0d4ebb4793b01e2e482a94cdd3e2a
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2018-05-15 10:14:40 +0100

    Add Tablet-friendly design

--- js/employees.js
+++ js/employees.js
@@ -15,10 +15,12 @@ fetchData("https://randomuser.me/api/?results=12&nat=gb")
 //Function to populate the Employees div with user data.
 function listEmployees(data) {
     const list = data.map(employee => `<div class="card">
-                                            <img src='${employee.picture.large}' alt>
-                                            <h3 class='name'>${employee.name.first[0].toUpperCase()}${employee.name.first.slice(1)} ${employee.name.last[0].toUpperCase()}${employee.name.last.slice(1)}</h3>
-                                            <h4 class="mail">${employee.email}</h4>
-                                            <span class="location">${employee.location.city[0].toUpperCase()}${employee.location.city.slice(1)}</span>
+                                            <img src='${employee.picture.medium}' alt>
+                                            <div class="info">
+                                                <h3 class='name'>${employee.name.first[0].toUpperCase()}${employee.name.first.slice(1)} ${employee.name.last[0].toUpperCase()}${employee.name.last.slice(1)}</h3>
+                                                <h4 class="mail">${employee.email}</h4>
+                                                <span class="location">${employee.location.city[0].toUpperCase()}${employee.location.city.slice(1)}</span>
+                                            </div>
                                         </div>`).join("");
     document.querySelector("#employees").innerHTML = list;
 };
