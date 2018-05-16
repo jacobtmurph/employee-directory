e504c1d0b374ec38bd9711a25935da643f56a3f3
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2018-05-16 07:49:51 +0100

    Add desktop design

--- js/employees.js
+++ js/employees.js
@@ -14,13 +14,16 @@ fetchData("https://randomuser.me/api/?results=12&nat=gb")
 
 //Function to populate the Employees div with user data.
 function listEmployees(data) {
-    const list = data.map(employee => `<div class="card">
+    const list = data.map(employee => `<a class="lightbox" href="https://randomuser.me/api/?seed=${employee.seed}">
+                                        <div class="card">
                                             <img src='${employee.picture.medium}' alt>
                                             <div class="info">
                                                 <h3 class='name'>${employee.name.first[0].toUpperCase()}${employee.name.first.slice(1)} ${employee.name.last[0].toUpperCase()}${employee.name.last.slice(1)}</h3>
                                                 <h4 class="mail">${employee.email}</h4>
                                                 <span class="location">${employee.location.city[0].toUpperCase()}${employee.location.city.slice(1)}</span>
                                             </div>
-                                        </div>`).join("");
+                                        </div>
+                                        </a>`).join("");
     document.querySelector("#employees").innerHTML = list;
 };
+
