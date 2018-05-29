40dec6705cab6c4b7d801e55a2d16f59c85b8dc4
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2018-05-29 10:43:58 +0100

    Add modal functionality

--- js/employees.js
+++ js/employees.js
@@ -14,16 +14,66 @@ fetchData("https://randomuser.me/api/?results=12&nat=gb")
 
 //Function to populate the Employees div with user data.
 function listEmployees(data) {
-    const list = data.map(employee => `<a class="lightbox" href="https://randomuser.me/api/?seed=${employee.seed}">
-                                        <div class="card">
+    const list = data.map(employee => `<div id="${employee.name.first}-card" class="card">
                                             <img src='${employee.picture.medium}' alt>
                                             <div class="info">
                                                 <h3 class='name'>${employee.name.first[0].toUpperCase()}${employee.name.first.slice(1)} ${employee.name.last[0].toUpperCase()}${employee.name.last.slice(1)}</h3>
                                                 <h4 class="mail">${employee.email}</h4>
                                                 <span class="location">${employee.location.city[0].toUpperCase()}${employee.location.city.slice(1)}</span>
-                                            </div>
-                                        </div>
-                                        </a>`).join("");
+                                                <div class="modal-info">
+                                                    <h4>------</h4>
+                                                    <h4 class="mobile">${employee.cell}</h4>
+                                                    <h4 class="address">${employee.location.street}, ${employee.location.postcode}</4>
+                                                    <h4 class="brithday">Birthday: ${employee.dob.slice(0, 10)}</h4>
+                                                </div>
+                                                </div>
+                                        </div>`).join("");
     document.querySelector("#employees").innerHTML = list;
 };
 
+
+//Function to create modal & display boxes
+function createModal(id) {
+    //Get the id of the base card
+    const card = document.getElementById(id);
+    
+    //Create the div with id "modal"
+    const modal = document.createElement("div");
+    modal.id = "modal";
+
+    //Set the content of the modal to the card content, with a close button
+    modal.innerHTML = `<div id="modal-content">
+                            ${card.innerHTML}
+                            <a href="#" id="close"><h4>Close</h4></a>
+                        </div>`;
+    //Display the modal
+    return document.body.prepend(modal);
+}
+
+//Add click event listener to the employees div 
+document.querySelector("#employees").addEventListener("click", (e) => {
+    //If the box is clicked
+    if (e.target.classList.contains("card")) {
+        //Create the required modal
+        createModal(e.target.id);
+
+        //If an element in the box is clicked
+    } else if (e.target.parentNode.classList.contains("card")) {
+        //Create the required modal
+        createModal(e.target.parentNode.id);
+
+        //if the info is clicked
+    } else if (e.target.parentNode.classList.contains("info")) {
+        //Create the required modal
+        createModal(e.target.parentNode.parentNode.id);
+    }
+
+    //If the close button is clicked
+    document.getElementById("close").addEventListener("click", (e) => {
+        //Prevent page reload
+        e.preventDefault();
+        
+        //Remove the modal
+        document.body.removeChild(document.querySelector("#modal"));
+    });
+});
